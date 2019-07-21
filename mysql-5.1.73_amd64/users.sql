8d738bf3c52c25f17ecbc45430f84174910690f2
Whitey McCarragher <matt.mccarragher@online-rewards.com>
2019-07-21 00:46:45 -0400

    working mysql legacy

---
+++ mysql-5.1.73_amd64/users.sql
@@ -0,0 +1,5 @@
+CREATE USER 'user'@'localhost' IDENTIFIED BY 'password';
+GRANT ALL PRIVILEGES ON *.* TO 'user'@'localhost' WITH GRANT OPTION;
+CREATE USER 'user'@'%' IDENTIFIED BY 'password';
+GRANT ALL PRIVILEGES ON *.* TO 'user'@'%' WITH GRANT OPTION;
+FLUSH PRIVILEGES;
